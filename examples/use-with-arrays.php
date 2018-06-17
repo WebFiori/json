86a3f13ec09b2a9d7dc7e4a013ca59f20f5f5d43
Ibrahim <ibinshikh@hotmail.com>
2018-06-17 09:37:04 +0300

    Example to show how to use JsonX with arrays

---
+++ examples/use-with-arrays.php
@@ -0,0 +1,31 @@
+<?php
+//show any errors
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+
+//load JsonX
+require_once '../JsonX.php';
+
+//initialize an object of the class JsonX
+$j = new JsonX();
+
+$arr = array(1,'1','1.4',5.8,'hello');
+$arr2 = array(
+    'name'=>'Ibrahim',100,
+    'age'=>'25','sex'=>'M',true,false
+);
+
+//add the array as is
+$j->add('basic-array', $arr);
+
+//adding the array as object
+$j->addArray('basic-array-as-object', $arr,TRUE);
+
+//adding the array as is
+$j->add('complicated-array', $arr2);
+
+//adding the array as object
+$j->addArray('complicated-array-as-object', $arr2,TRUE);
+echo $j;
