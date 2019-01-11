3e40377beab9fd8f3890e5635a9c054963606781
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-01-11 22:04:15 +0300

    Updated licesnse info.

--- JsonX.php
+++ JsonX.php
@@ -2,7 +2,7 @@
 /*
  * The MIT License
  *
- * Copyright 2019 Ibrahim, JsonX library v1.4.2
+ * Copyright 2019 Ibrahim, JsonX library.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
