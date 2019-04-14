53f1a167d0c6b313b7808c7bc3d38caecc1a1155
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-04-15 02:17:27 +0300

    Create Obj1.php

---
+++ tests/Obj1.php
@@ -0,0 +1,20 @@
+<?php
+namespace jsonx\tests;
+use jsonx\JsonI;
+use jsonx\JsonX;
+/**
+ * Description of Obj1
+ *
+ * @author Ibrahim
+ */
+class Obj1 extends Obj0 implements JsonI{
+    //put your code here
+    public function toJSON() {
+        $j = new JsonX();
+        $j->add('property-00', $this->getProperty00());
+        $j->add('property-01', $this->getProperty01());
+        $j->add('property-02', $this->getProperty02());
+        return $j;
+    }
+
+}
