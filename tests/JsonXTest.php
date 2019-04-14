59643a3a1d2af3e78236ba2256a2457108ab623f
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-04-15 02:17:19 +0300

    Update JsonXTest.php

--- tests/JsonXTest.php
+++ tests/JsonXTest.php
@@ -1,8 +1,81 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use jsonx\JsonX;
+use jsonx\tests\Obj0;
+use jsonx\tests\Obj1;
 
 class JsonXTest extends TestCase{
+    /**
+     * @test
+     */
+    public function testAddNumber00() {
+        $j = new JsonX();
+        $j->addNumber('   number', 33);
+        $this->assertEquals('{"number":33}',$j.'');
+    }
+    /**
+     * @test
+     */
+    public function testAddBoolean00() {
+        $j = new JsonX();
+        $j->addBoolean('bool ', true);
+        $this->assertEquals('{"bool":true}',$j.'');
+    }
+    /**
+     * @test
+     */
+    public function testAddArray00() {
+        $j = new JsonX();
+        $arr = array();
+        $j->addArray('arr', $arr);
+        $this->assertEquals('{"arr":{}}',$j.'');
+    }
+    /**
+     * @test
+     */
+    public function testAddArray01() {
+        $j = new JsonX();
+        $arr = array(1,"Hello",true,NAN,null,99.8,INF);
+        $j->addArray('arr', $arr);
+        $this->assertEquals('{"arr":{"0":1, "1":"Hello", "2":true, "3":"NAN", "4":null, "5":99.8, "6":"INF"}}',$j.'');
+    }
+    /**
+     * @test
+     */
+    public function testAddArray02() {
+        $j = new JsonX();
+        $arr = array(1,1.5,"Hello",true,NAN,null,INF);
+        $j->addArray('arr', $arr,false);
+        $this->assertEquals('{"arr":[1, 1.5, "Hello", true, "NAN", null, "INF"]}',$j.'');
+    }
+    /**
+     * @test
+     */
+    public function testAddArray03() {
+        $j = new JsonX();
+        $arr = array("number"=>1,"Hello"=>"world!","boolean"=>true,NAN,null);
+        $j->addArray('arr', $arr);
+        $this->assertEquals('{"arr":{"number":1, "Hello":"world!", "boolean":true, "0":"NAN", "1":null}}',$j.'');
+    }
+    /**
+     * @test
+     */
+    public function testAddObj00() {
+        $j = new JsonX();
+        $obj = new Obj0('Hello', 0, true, null, 'he');
+        $j->addObject('object', $obj);
+        $this->assertEquals('{"object":{"prop-0":"Hello","prop-1":0,"prop-2":true,"prop-3":"he"}}',$j.'');
+    }
+    
+    /**
+     * @test
+     */
+    public function testAddObj01() {
+        $j = new JsonX();
+        $obj = new Obj1('Hello', 0, true, null, 'he');
+        $j->addObject('object', $obj);
+        $this->assertEquals('{"object":{"property-00":"Hello","property-01":0,"property-02":true}}',$j.'');
+    }
     /**
      * @test
      */
@@ -36,5 +109,15 @@ class JsonXTest extends TestCase{
         $str = 'Path: "C:/Windows/Media/onestop.midi"\n';
         $result = JsonX::escapeJSONSpecialChars($str);
         $this->assertEquals('Path: \"C:\/Windows\/Media\/onestop.midi\"\\\\n',$result);
+    }
+    /**
+     * @test
+     */
+    public function testEscJSonSpecialChars02() {
+        $str = '\tI\'m good. But "YOU" are "Better".\r\n'
+                . '\\An inline comment is good.';
+        $result = JsonX::escapeJSONSpecialChars($str);
+        $this->assertEquals('\\\\tI\'m good. But \"YOU\" are \"Better\".\\\\r\\\\n'
+                . '\\\\An inline comment is good.',$result);
     }
 }
