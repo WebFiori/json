534885cfdae662960cb611723ff65a10a4493498
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-04-14 17:30:36 +0300

    Update JsonXTest.php

--- tests/JsonXTest.php
+++ tests/JsonXTest.php
@@ -3,9 +3,38 @@ use PHPUnit\Framework\TestCase;
 use jsonx\JsonX;
 
 class JsonXTest extends TestCase{
-    public function test1(){
+    /**
+     * @test
+     */
+    public function testAddStringTest00(){
         $j = new JsonX();
-        $isAdded = $j->addString('hello', 'Hello World!');
-        $this->assertEquals($isAdded,TRUE);
+        $this->assertFalse($j->addString('', 'Hello World!'));
+        $this->assertFalse($j->addString('  ', 'Hello World!'));
+        $this->assertFalse($j->addString("\n", 'Hello World!'));
+        $this->assertEquals('{}',$j.'');
+    }
+    /**
+     * @test
+     */
+    public function testAddStringTest01(){
+        $j = new JsonX();
+        $this->assertTrue($j->addString('hello', 'Hello World!'));
+        $this->assertEquals('{"hello":"Hello World!"}',$j.'');
+    }
+    /**
+     * @test
+     */
+    public function testEscJSonSpecialChars00() {
+        $str = 'I\'m "Good".';
+        $result = JsonX::escapeJSONSpecialChars($str);
+        $this->assertEquals('I\'m \"Good\".',$result);
+    }
+    /**
+     * @test
+     */
+    public function testEscJSonSpecialChars01() {
+        $str = 'Path: "C:/Windows/Media/onestop.midi"\n';
+        $result = JsonX::escapeJSONSpecialChars($str);
+        $this->assertEquals('Path: \"C:\/Windows\/Media\/onestop.midi\"\\\\n',$result);
     }
 }
