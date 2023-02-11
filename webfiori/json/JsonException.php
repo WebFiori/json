b41e083d84ebc63a2a30f39f0321afb2982dce70
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-02-11 22:15:02 +0300

    Create JsonException.php

---
+++ webfiori/json/JsonException.php
@@ -0,0 +1,10 @@
+<?php
+namespace webfiori\json;
+
+use Exception;
+/**
+ * A class which is thrown to indicate that something is incorrect in JSON.
+ *
+ * @author Ibrahim
+ */
+class JsonException extends Exception {}
