83214dca172db3b7e8fe093a22b88c6bfbfccd1b
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-02-11 22:14:46 +0300

    Update loader.php

--- tests/loader.php
+++ tests/loader.php
@@ -40,6 +40,7 @@ if (explode($DS, $rootDirTrimmed)[0] == 'home') {
 }
 define('ROOT', $rootDir);
 echo 'Root Directory: \''.$rootDir.'\'.'."\n";
+require_once $rootDir.'webfiori'.$DS.'json'.$DS.'JsonException.php';
 require_once $rootDir.'webfiori'.$DS.'json'.$DS.'JsonConverter.php';
 require_once $rootDir.'webfiori'.$DS.'json'.$DS.'CaseConverter.php';
 require_once $rootDir.'webfiori'.$DS.'json'.$DS.'Property.php';
